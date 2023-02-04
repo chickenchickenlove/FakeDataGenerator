d17e7956dbec67305057d167e8e0db184fce7ed1
chickenchickenlove <ojt90902@naver.com>
2023-02-04 11:49:51 +0900

    git init

---
+++ src/main/java/org/hello/chickenchickenlove/fakeDataGenerator/FakeDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.hello.chickenchickenlove.fakeDataGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeDataGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hello/chickenchickenlove/fakeDataGenerator/FakeDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hello.chickenchickenlove.fakeDataGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeDataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
